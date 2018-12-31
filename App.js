85d5c4aa6c0913b77065b11415c581a47c587870
anuradharg <16790236+anuradharg@users.noreply.github.com>
2018-12-31 22:42:41 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -3,12 +3,19 @@ import './App.css';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas:[
+      {name:'Ryu', age:25 , belt:'black', id:1 },
+      {name:'Yoshi', age:30 , belt:'green', id:2 },
+      {name:'Crystal', age:28 , belt:'yellow', id:3 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My First React App!</h1>
         <p>Welcome :)))</p>
-        <Ninjas name="Ryu" age="25" belt="black"/>
+        <Ninjas ninjas = {this.state.ninjas} />
       </div>  
     );
   }
